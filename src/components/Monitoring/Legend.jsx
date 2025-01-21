c8b52b83157a5dfda3c7acd9206cd5977827b714
yuna872 <yunae426@gmail.com>
2025-01-22 02:45:26 +0900

    [refactor] legend 컴포넌트 분리

---
+++ src/components/Monitoring/Legend.jsx
@@ -0,0 +1,46 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+export const STATUS = {
+  정상: "green",
+  오류: "red",
+  미접속: "yellow",
+};
+
+const Legend = () => {
+  return (
+    <Stack
+      sx={{
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+        padding: "10px",
+        gap: "10px",
+        borderRadius: "5px",
+        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography sx={{ fontSize: "16px" }}>범례</Typography>
+      <Stack sx={{ gap: "7px" }}>
+        {Object.entries(STATUS).map(([key, color]) => {
+          return (
+            <Stack
+              sx={{ flexDirection: "row", gap: "5px", alignItems: "center" }}
+              key={key}
+            >
+              <Box
+                sx={{
+                  display: "inline-block",
+                  width: "15px",
+                  height: "15px",
+                  borderRadius: "100%",
+                  backgroundColor: `${color}`,
+                }}
+              />
+              <Typography sx={{ fontSize: "14px" }}>{key}</Typography>
+            </Stack>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Legend;
--- src/pages/Monitoring/index.jsx
+++ src/pages/Monitoring/index.jsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from "react";
-import "./Main.css";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../../components/Header";
+import {
+  Box,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { green } from "@mui/material/colors";
+import Legend, { STATUS } from "../../components/Monitoring/Legend";
 
 const Monitoring = () => {
   const [signalList, setSignalList] = useState([]);
   const [map, setMap] = useState(null);
   const [clusterer, setClusterer] = useState(null);
-  const [roadview, setRoadview] = useState(null);
-  const [roadviewClient, setRoadviewClient] = useState(null);
   const [isRoadviewActive, setIsRoadviewActive] = useState(false);
   const [roadviewPosition, setRoadviewPosition] = useState(null);
 
-  const statusColors = {
-    정상: "green",
-    오류: "red",
-    미접속: "yellow",
-  };
+  const mapContainerRef = useRef(null);
+  const roadviewContainerRef = useRef(null);
 
   // 지도 및 로드뷰 초기화
   useEffect(() => {
     if (typeof window.kakao !== "undefined") {
-      const mapContainer = document.getElementById("map");
-      const roadviewContainer = document.getElementById("roadview");
       const mapOption = {
         center: new window.kakao.maps.LatLng(37.2803, 127.0181),
         level: 6,
       };
 
-      const mapInstance = new window.kakao.maps.Map(mapContainer, mapOption);
+      const mapInstance = new window.kakao.maps.Map(
+        mapContainerRef.current,
+        mapOption
+      );
       setMap(mapInstance);
 
-      // 클러스터러 생성
       const clustererInstance = new window.kakao.maps.MarkerClusterer({
         map: mapInstance,
         gridSize: 35,
@@ -49,14 +55,6 @@ const Monitoring = () => {
         ],
       });
       setClusterer(clustererInstance);
-
-      // 로드뷰 및 로드뷰 클라이언트 객체 초기화
-      const roadviewInstance = new window.kakao.maps.Roadview(
-        roadviewContainer
-      );
-      const roadviewClientInstance = new window.kakao.maps.RoadviewClient();
-      setRoadview(roadviewInstance);
-      setRoadviewClient(roadviewClientInstance);
     } else {
       console.error("Kakao map library not loaded.");
     }
@@ -89,7 +87,7 @@ const Monitoring = () => {
                 signal.longitude
               );
               const markerImage = new window.kakao.maps.MarkerImage(
-                `/images/marker-${statusColors[signal.status]}-icon.png`,
+                `/images/marker-${STATUS[signal.status]}-icon.png`,
                 new window.kakao.maps.Size(20, 35)
               );
 
@@ -121,80 +119,67 @@ const Monitoring = () => {
 
   // 로드뷰 보기 버튼 클릭 시 위치 설정
   const toggleView = () => {
-    if (roadviewClient && roadviewPosition) {
-      // 로드뷰의 위치를 마커 클릭 위치로 설정
-      roadviewClient.getNearestPanoId(roadviewPosition, 50, function (panoId) {
-        if (panoId) {
-          roadview.setPanoId(panoId, roadviewPosition); // 로드뷰 파노 ID와 위치 설정
-        }
-      });
-    }
     setIsRoadviewActive((prev) => !prev);
   };
 
   return (
     <div>
       <Header />
+      <Stack sx={{ flexDirection: "row" }}>
+        {/* 신호기 목록 */}
+        <Stack>
+          <Typography>신호기 목록</Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>주소</TableCell>
+                <TableCell>버튼 클릭</TableCell>
+                <TableCell>위치 안내</TableCell>
+                <TableCell>신호 안내</TableCell>
+                <TableCell>생성일</TableCell>
+                <TableCell>상태</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {signalList.map((signal) => (
+                <TableRow key={signal.sensorId} className="signal-card">
+                  <TableCell>{signal.sensorId}</TableCell>
+                  <TableCell>{signal.address}</TableCell>
+                  <TableCell>{signal.buttonClickCount}</TableCell>
+                  <TableCell>{signal.locationGuideCount}</TableCell>
+                  <TableCell>{signal.signalGuideCount}</TableCell>
+                  <TableCell>
+                    {new Date(signal.createdAt).toLocaleString()}
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      className={`status-icon ${STATUS[signal.status]}`}
+                    ></Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Stack>
+        {/* 카카오맵 */}
+      </Stack>
       <div>
-        <aside className="lnb">
-          <div className="lnb-header">
-            <h2>신호기 목록</h2>
-          </div>
-          <div className="signal-index">
-            <div>ID</div>
-            <div>주소</div>
-            <div>버튼 클릭</div>
-            <div>위치 안내</div>
-            <div>신호 안내</div>
-            <div>생성일</div>
-            <div>상태</div>
-          </div>
-          <div className="signal-list">
-            {signalList.map((signal) => (
-              <div key={signal.sensorId} className="signal-card">
-                <div>{signal.sensorId}</div>
-                <div>{signal.address}</div>
-                <div>{signal.buttonClickCount}</div>
-                <div>{signal.locationGuideCount}</div>
-                <div>{signal.signalGuideCount}</div>
-                <div>{new Date(signal.createdAt).toLocaleString()}</div>
-                <div>
-                  <span
-                    className={`status-icon ${statusColors[signal.status]}`}
-                  ></span>
-                </div>
-              </div>
-            ))}
-          </div>
-        </aside>
         <section className="map-container">
           <div
+            ref={mapContainerRef}
             style={{ display: isRoadviewActive ? "none" : "block" }}
-            id="map"
             className="map"
           ></div>
           <div
+            ref={roadviewContainerRef}
             style={{ display: isRoadviewActive ? "block" : "none" }}
-            id="roadview"
             className="roadview"
           ></div>
           <button onClick={toggleView} className="toggle-view">
             {isRoadviewActive ? "지도 보기" : "로드뷰 보기"}
           </button>
-          <div className="legend">
-            <h3>범례</h3>
-            <ul>
-              <li>
-                <span className="legend-circle green"></span> 정상
-              </li>
-              <li>
-                <span className="legend-circle red"></span> 오류
-              </li>
-              <li>
-                <span className="legend-circle yellow"></span> 미접속
-              </li>
-            </ul>
-          </div>
+          <Legend />
         </section>
       </div>
     </div>
