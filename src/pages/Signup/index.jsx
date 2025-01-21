665ac2737564503ab44fe2fdd10fa5692373f53f
yuna872 <yunae426@gmail.com>
2025-01-22 01:41:48 +0900

    [refactor] 회원가입 mui 적용 및 불필요한 파일 삭제

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import './Signup.css';
+import React, { useState } from "react";
+import { Button, MenuItem, Stack, Typography } from "@mui/material";
+import { TextField } from "../../components/TextField";
+import { Select } from "../../components/Select";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    role: 'USER',
+    username: "",
+    password: "",
+    role: "USER",
   });
 
   const handleChange = (e) => {
@@ -18,59 +20,91 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const { username, password } = formData;
+    if (username === "" || password === "") {
+      alert("아이디, 비밀번호를 입력해주세요");
+      return;
+    }
+
     try {
-      const response = await fetch('http://localhost:8080/auth/sign-up', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("http://localhost:8080/auth/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
 
       const data = await response.json();
-      if (data.code === 'SUCCESS') {
-        alert('회원가입 성공!');
-        window.location.href = '/login';
+      if (data.code === "SUCCESS") {
+        alert("회원가입 성공!");
+        window.location.href = "/login";
       } else {
-        alert(data.message || '회원가입 실패');
+        alert(data.message || "회원가입 실패");
       }
     } catch (error) {
-      alert('회원가입 중 오류가 발생했습니다.');
+      alert("회원가입 중 오류가 발생했습니다.");
     }
   };
 
   return (
-    <div className="signup-container">
-      <form onSubmit={handleSubmit}>
-        <h2>회원가입</h2>
-        <label>
-          아이디:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          암호:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label>
-          관리등급:
-          <select name="role" value={formData.role} onChange={handleChange}>
-            <option value="USER">USER</option>
-            <option value="ADMIN">ADMIN</option>
-          </select>
-        </label>
-        <button type="submit">회원가입</button>
-      </form>
-    </div>
+    <Stack
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f9f9f9",
+      }}
+    >
+      <Stack
+        sx={{
+          width: "300px",
+          padding: "20px",
+          margin: "auto",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          alignItems: "center",
+          gap: "20px",
+          backgroundColor: "#fff",
+        }}
+      >
+        <Typography sx={{ fontSize: "24px", fontWeight: 600 }}>
+          회원가입
+        </Typography>
+        <Stack sx={{ gap: "10px", width: "100%" }}>
+          <Stack>
+            <Typography>아이디</Typography>
+            <TextField
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </Stack>
+          <Stack>
+            <Typography>암호</Typography>
+            <TextField
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </Stack>
+          <Stack>
+            <Typography>관리등급</Typography>
+            <Select name="role" value={formData.role} onChange={handleChange}>
+              <MenuItem value="USER">USER</MenuItem>
+              <MenuItem value="ADMIN">ADMIN</MenuItem>
+            </Select>
+          </Stack>
+        </Stack>
+        <Button
+          onClick={handleSubmit}
+          variant="contained"
+          sx={{ width: "100%" }}
+        >
+          회원가입
+        </Button>
+      </Stack>
+    </Stack>
   );
 };
 
