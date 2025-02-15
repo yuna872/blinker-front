25680fe83442aca5f82965012e57cea6b83321bc
yuna872 <yunae426@gmail.com>
2025-02-15 13:52:15 +0900

    [feat] 인포윈도우 수정

--- src/pages/Monitoring/index.jsx
+++ src/pages/Monitoring/index.jsx
@@ -190,11 +190,47 @@ const Monitoring = () => {
                           onClick={() => handleClickMarker(sensor)}
                         >
                           {selected && (
-                            <span
-                              sx={{ fontSize: "14px", textAlign: "center" }}
+                            <Stack
+                              sx={{
+                                width: "100%",
+                                "& > p": { fontSize: "11px" },
+                              }}
                             >
-                              {sensor.address}
-                            </span>
+                              <Typography>
+                                해당 기기 번호 (마스터 xX번)
+                              </Typography>
+                              <Stack
+                                sx={{
+                                  "& > p": { fontSize: "11px" },
+                                }}
+                              >
+                                <Typography>
+                                  기기 위치: {sensor.address}
+                                </Typography>
+                                <Typography>
+                                  마지막 작동시간: {"마지막 작동시간"}
+                                </Typography>
+                                <Typography>
+                                  동작 상태: {sensor.status}
+                                </Typography>
+                              </Stack>
+                              <Typography sx={{ fontSize: "10px" }}>
+                                -----------
+                              </Typography>
+                              <Typography sx={{ fontSize: "10px" }}>
+                                메모
+                              </Typography>
+                              <input
+                                type="text"
+                                id="info-window-memo"
+                                style={{
+                                  border: "none",
+                                  outline: "none",
+                                  fontSize: "10px",
+                                }}
+                                placeholder="간단한 메모 작성"
+                              />
+                            </Stack>
                           )}
                         </MapMarker>
                       );
