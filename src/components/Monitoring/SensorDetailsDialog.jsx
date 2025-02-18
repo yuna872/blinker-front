795ebd8347f44fec0f9bd3d9855165b3c2ee9f5e
yuna872 <yunae426@gmail.com>
2025-02-19 02:12:46 +0900

    [feat] 센서 상세 다이얼로그 - 장애 정보 테이블 추가

--- src/components/Monitoring/SensorDetailsDialog.jsx
+++ src/components/Monitoring/SensorDetailsDialog.jsx
@@ -10,16 +10,17 @@ import {
 import { Close } from "@mui/icons-material";
 import { useState } from "react";
 import { theme } from "@styles/theme";
+import FaultInfoTabPanel from "./FaultInfoTabPanel";
 
 const SensorDetailsDialog = ({ open, handleClose }) => {
-  const [tabValue, setTabValue] = useState("");
+  const [tabValue, setTabValue] = useState("FAULT_INFO");
   const handleChangeTabValue = (_, newValue) => {
     setTabValue(newValue);
   };
   return (
     <Dialog onClose={handleClose} open={open} maxWidth="md" fullWidth>
       <Stack sx={{ backgroundColor: theme.palette.primary.main }}>
-        <DialogTitle sx={{ m: 0, p: 2, color: "white", fontSize:'18px' }}>
+        <DialogTitle sx={{ m: 0, p: 2, color: "white", fontSize: "18px" }}>
           신호기 + 아이디 + 마스터 or 슬레이브(번호) - 위치
         </DialogTitle>
       </Stack>
@@ -34,19 +35,23 @@ const SensorDetailsDialog = ({ open, handleClose }) => {
       >
         <Close />
       </IconButton>
-      <DialogContent>
+      <DialogContent sx={{padding : '15px'}}>
         <Tabs
           value={tabValue}
           onChange={handleChangeTabValue}
           aria-label="basic tabs example"
+          sx={{ "& .MuiTab-root": { fontSize: "16px" } }}
+          variant='fullWidth'
         >
-          <Tab label="장애 정보" value="장애 정보" />
-          <Tab label="설정" value="설정" />
-          <Tab label="상태 이력" value="상태 이력" />
+          <Tab label="장애 정보" value="FAULT_INFO" />
+          <Tab label="설정" value="SETTINGS" />
+          <Tab label="상태 이력" value="STATUS_LOG" />
         </Tabs>
-        {tabValue === "장애 정보" && <Stack>장애 정보</Stack>}
-        {tabValue === "설정" && <Stack>설정</Stack>}
-        {tabValue === "상태 이력" && <Stack>상태 이력</Stack>}
+        <Stack sx={{ paddingY: "15px", height: "400px", overflow: "hidden" }}>
+          {tabValue === "FAULT_INFO" && <FaultInfoTabPanel />}
+          {tabValue === "SETTINGS" && <Stack>설정</Stack>}
+          {tabValue === "STATUS_LOG" && <Stack>상태 이력</Stack>}
+        </Stack>
       </DialogContent>
     </Dialog>
   );
