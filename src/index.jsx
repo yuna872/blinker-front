ab5ea793d81d80a68d0836a6e4e41815d38bd7a2
yuna872 <yunae426@gmail.com>
2025-02-21 00:00:31 +0900

    [feat] cookiesProvider 적용

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client"; // createRoot 메서드를 가져오기
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./styles/theme";
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // ReactDOM.createRoot 사용
 root.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </ThemeProvider>
 );
