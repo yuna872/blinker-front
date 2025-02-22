55e49ba1c02735199697e3b204edb7b417f102b1
yuna872 <yunae426@gmail.com>
2025-02-22 21:34:53 +0900

    [chore] query client provider 위치 수정

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,16 @@ import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./styles/theme";
 import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // ReactDOM.createRoot 사용
 root.render(
-  <ThemeProvider theme={theme}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
 );
