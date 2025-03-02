d4d24da300a3a91bc6cf1e2d9da5020ffa0cb12b
yuna872 <yunae426@gmail.com>
2025-03-02 22:13:06 +0900

    [feat] 그룹 페이지 유저별 신호기 목록 조회 api 연동

--- src/components/Group/SensorList.jsx
+++ src/components/Group/SensorList.jsx
@@ -1,20 +1,29 @@
+import { useGetUserSensorGroups } from "@apis/sensor/useGetUserSensorGroups";
 import {
   TableHeaderStyle,
   TableRowStyle,
 } from "@components/Sensors/SensorList";
 import Title from "@components/Title";
-import {
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Star } from "@mui/icons-material";
+import { Stack } from "@mui/material";
 import { grey } from "@mui/material/colors";
-import { dummySignalLights } from "@pages/User/Monitoring/dummy";
+import { setSelectedSensorState } from "@store/selectedSensorSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SensorList = () => {
+  const selectedSensor = useSelector((state) => state.selectedSensor);
+  const selectedUser = useSelector((state) => state.selectedUser);
+  const dispatch = useDispatch();
+  const { data: sensorGroups } = useGetUserSensorGroups(
+    selectedUser?.appUserId
+  );
+
+  console.log(sensorGroups);
+
+  const handleClickSensor = (sensor) => {
+    dispatch(setSelectedSensorState(sensor));
+  };
+
   return (
     <Stack
       sx={{
@@ -25,30 +34,82 @@ const SensorList = () => {
       <Title title="센서 목록" />
       <Stack
         sx={{
-          padding: "10px",
-          flex: "1",
-          overflowY: "auto",
+          margin: "10px",
+          border: `1px solid ${grey[200]}`,
         }}
       >
-        <Table>
-          <TableHead>
-            <TableRow sx={TableHeaderStyle}>
-              <TableCell>ID</TableCell>
-              <TableCell>위치</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dummySignalLights.map((item) => {
+        {/* Header */}
+        <Stack sx={TableHeaderStyle}>
+          <Stack sx={{ width: "40px", maxWidth: "40px" }}>ID</Stack>
+          <Stack sx={{ width: "280px", maxWidth: "280px" }}>주소</Stack>
+        </Stack>
+        {/* Body */}
+        {sensorGroups && (
+          <Stack
+            sx={{
+              backgroundColor: grey[300],
+              overflowY: "auto",
+            }}
+          >
+            {sensorGroups?.map((group) => {
               return (
-                <TableRow sx={TableRowStyle} key={item.sensorId}>
-                  {/* TODO: Id 수정 필요 */}
-                  <TableCell>00000741702c1ffffe54c0c3</TableCell>
-                  <TableCell>{item.address}</TableCell>
-                </TableRow>
+                <Stack key={group.sensorGroupId}>
+                  <Stack
+                    sx={{
+                      ...TableRowStyle,
+                      "&:hover": {
+                        backgroundColor: "none",
+                      },
+                    }}
+                  >
+                    <Stack sx={{ width: "30px", maxWidth: "30px" }}>
+                      {group.order}
+                    </Stack>
+                    <Stack sx={{ width: "210px", maxWidth: "210px" }}>
+                      {group.sensorGroupId}
+                    </Stack>
+                    <Stack>{`(SSID) ${group.ssid}`}</Stack>
+                  </Stack>
+                  {group.sensors.map((sensor) => {
+                    const selected =
+                      sensor.sensorId === selectedSensor?.sensorId;
+                    return (
+                      <Stack
+                        spacing={1}
+                        key={sensor.sensorId}
+                        sx={{
+                          ...TableRowStyle,
+                          marginLeft: "15px",
+                          backgroundColor: selected ? grey[100] : "white",
+                          flexDirection: "row",
+                        }}
+                        onClick={() => handleClickSensor(sensor)}
+                      >
+                        <Stack sx={{ width: "40px", maxWidth: "40px" }}>
+                          {group.order}-{sensor.groupPositionNumber}
+                        </Stack>
+                        <Stack
+                          sx={{
+                            width: "280px",
+                            maxWidth: "280px",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: "5px",
+                          }}
+                        >
+                          {!sensor.groupPositionNumber && (
+                            <Star sx={{ width: "15px", color: grey[700] }} />
+                          )}
+                          {sensor.address}
+                        </Stack>
+                      </Stack>
+                    );
+                  })}
+                </Stack>
               );
             })}
-          </TableBody>
-        </Table>
+          </Stack>
+        )}
       </Stack>
     </Stack>
   );
