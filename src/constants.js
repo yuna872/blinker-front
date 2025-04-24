03e3e2b22c8c8608c5026cad9b241a8bd623ebc9
yuna872 <yunae426@gmail.com>
2025-04-24 23:39:40 +0900

    [feat] 상태 이력 테이블 수정

--- src/constants.js
+++ src/constants.js
@@ -1,67 +1,3 @@
-export const dummyUsers = [
-  { id: 0, userId: "admin", userName: "admin" },
-  {
-    id: 1,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 2,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 3,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 4,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 5,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 6,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 7,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 8,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 9,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 10,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 11,
-    userId: "test",
-    userName: "test",
-  },
-  {
-    id: 12,
-    userId: "test",
-    userName: "test",
-  },
-];
-
 export const FAULTS = {
   "Front Cover Open": "전면 커버 감지",
   "235.3MHz Receiver Fault": "235 채널 신호",
@@ -91,8 +27,27 @@ export const VOLUME_SETTINGS = [
 ];
 
 export const SILENT_SETTINGS = [
-  'Female Mute 1',
-  'Female Mute 2',
-  'Male Mute 1',
-  'Male Mute 2',
-]
+  "Female Mute 1",
+  "Female Mute 2",
+  "Male Mute 1",
+  "Male Mute 2",
+];
+
+export const CMD = {
+  HEX: "구분",
+  73: "설정변경",
+  67: "상태응답",
+  61: "SSID수정",
+  77: "SSID응답",
+  76: "교차로명 수정",
+  65: "요류 이벤트 발생",
+  72: "정기보고",
+  74: "최초부팅",
+  62: "버튼 이벤트 발생",
+  "6D": "위치 리모컨 이벤트 발생",
+  "6E": "신호 리모컨 이벤트 발생",
+  "6F": "235 신호 이벤트 발생",
+  63: "오류 복구 이벤트",
+  70: "GPS 좌표 저장",
+  71: "GPS 좌표 송신",
+};
