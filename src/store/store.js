f6dbecc18b74b9dbddb32e1221b1abdd96ad77d0
yuna872 <yunae426@gmail.com>
2025-04-29 17:05:48 +0900

    unused import test

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import selectedSensorReducer from '@store/selectedSensorSlice';
 import mapPositionReducer from '@store/mapPositionSlice';
 import selectedUserReducer from '@store/selectedUserSlice';
-import UserTable from '@pages/Admin/Group/components/UserTable';
 
 export const store = configureStore({
   reducer: {
