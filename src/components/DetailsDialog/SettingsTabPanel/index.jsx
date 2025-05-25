bfe9cfe54786cd1f5201e4004c9d8dfd4e160d89
yuna872 <yunae426@gmail.com>
2025-05-25 21:03:32 +0900

    [feat] 로그 수집 api 연동

--- src/components/DetailsDialog/SettingsTabPanel/index.jsx
+++ src/components/DetailsDialog/SettingsTabPanel/index.jsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { usePutSensor } from "@apis/sensor/usePutSensor";
 import { showToast } from "@utils/toast";
 import { usePutSensorLog } from "@apis/sensor/usePutSensorLog";
+import { usePostSensorLog } from "@apis/sensor/usePostSensorLog";
 
 const SettingsTabPanel = () => {
   const selectedSensor = useSelector((state) => state.selectedSensor);
@@ -102,6 +103,21 @@ const SettingsTabPanel = () => {
     }
   };
 
+  const { mutateAsync: postSensorLog } = usePostSensorLog();
+  const handleClickPostLog = async () => {
+    if (!selectedSensor) return;
+    const { sensorGroupId } = selectedSensor;
+    try {
+      await postSensorLog({ sensorGroupId }).then((data) => {
+        if (data.code === "SUCCESS") {
+          showToast.success("처리되었습니다.");
+        }
+      });
+    } catch (error) {
+      showToast.error(error?.response?.data?.message);
+    }
+  };
+
   const { mutateAsync: putSensorLog } = usePutSensorLog();
   const handleClickGCommand = async () => {
     if (!selectedSensor) return;
@@ -109,7 +125,7 @@ const SettingsTabPanel = () => {
     try {
       await putSensorLog({ sensorGroupId }).then((data) => {
         if (data.code === "SUCCESS") {
-          showToast.success("완료되었습니다.");
+          showToast.success("처리되었습니다.");
         }
       });
     } catch (error) {
@@ -142,6 +158,9 @@ const SettingsTabPanel = () => {
         <Stack
           sx={{ flexDirection: "row", justifyContent: "center", gap: "20px" }}
         >
+          <Button variant="contained" onClick={handleClickPostLog}>
+            로그 수집
+          </Button>
           <Button variant="contained" onClick={handleClickGCommand}>
             G명령
           </Button>
